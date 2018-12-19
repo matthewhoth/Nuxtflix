5921e717b1925ae315765e93d2797d96b4b6e9a0
Matthew Hoth <matthewhoth@gmail.com>
2018-12-19 20:51:39 +0800

    Faux login

--- store/index.js
+++ store/index.js
@@ -1,20 +1,35 @@
 import Cookies from "js-cookie";
 
 export const state = () => ({
+  accountDetails: [],
   user: ""
 });
 
 export const mutations = {
+  REGISTER_ACCOUNT(state, accountDetails) {
+    state.accountDetails = accountDetails;
+  },
+
   SET_USER(state, user) {
     state.user = user;
   }
 };
 
 export const actions = {
+  async registerAccount({ commit }, accountDetails) {
+    Cookies.set("accountDetails", accountDetails, { expires: 365 });
+    commit("REGISTER_ACCOUNT", accountDetails);
+  },
+
   async setUser({ commit }, user) {
     Cookies.set("user", user, { expires: 365 });
-    Cookies.get("user");
     commit("SET_USER", user);
+  },
+
+  async userLogout({ commit }) {
+    Cookies.remove("user");
+    commit("REGISTER_ACCOUNT", []);
+    commit("SET_USER", "");
   },
 
   nuxtClientInit({ commit }) {
