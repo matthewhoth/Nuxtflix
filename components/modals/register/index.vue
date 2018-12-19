ed89efd8ed00bf1fb4f5004bc29a7a0badbc80cc
Matthew Hoth <matthewhoth@gmail.com>
2018-12-19 20:03:29 +0800

    Outline the register modal

---
+++ components/modals/register/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="modal_overlay">
+    <div class="modal_wrapper">
+      <div class="modal_container">
+        <form>
+          <input type="text" placeholder="Username">
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
