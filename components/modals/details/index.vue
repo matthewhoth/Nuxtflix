636fd927b6e0bc4d9f5cd8cdf9c5c6f49f901064
Matthew Hoth <matthewhoth@gmail.com>
2018-12-30 17:33:55 +0800

    Create a movies details modal

---
+++ components/modals/details/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="modal_overlay">
+    <div class="modal_wrapper">
+      <div class="modal_container" style="min-width: 35rem" v-click-outside="onClickOutside">
+        <div class="form_header">
+          <span>{{movieData.name}} ({{movieData.year}})</span>
+          <div class="modal_close">
+            <i
+              class="icon-circle-rounded-full click click_link"
+              @click="$emit('closeMoviesDetailsModal')"
+            ></i>
+          </div>
+        </div>
+        <div>Tester</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Cookies from "js-cookie";
+import clickOutside from "@/directives/clickOutside";
+export default {
+  directives: {
+    clickOutside
+  },
+  props: {
+    movieData: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      hasAccount: true,
+      username: "",
+      usernameValidation: false,
+      password: "",
+      passwordValidation: false
+    };
+  },
+  computed: {
+    validateForm() {}
+  },
+  mounted() {},
+  methods: {
+    onClickOutside() {
+      this.$emit("closeMoviesDetailsModal");
+    }
+  }
+};
+</script>
