b432861d888b5211566e609efd768e17b2ad0a41
Matthew Hoth <matthewhoth@gmail.com>
2019-01-06 22:15:34 +0800

    Update the API to TMDB

--- components/movies/movies-list/index.vue
+++ components/movies/movies-list/index.vue
@@ -24,7 +24,6 @@
 import movieCard from "@/components/ui/movie-card";
 import rateMovieModal from "@/components/modals/rating/index";
 import movieDetailsModal from "@/components/modals/details/index";
-import listOfMovies from "@/components/movies/movies-list/movies-list";
 
 export default {
   components: {
@@ -34,27 +33,28 @@ export default {
   },
   data() {
     return {
-      moviesList: listOfMovies,
+      moviesList: [],
       ratingModalState: false,
       movieDetailsModalState: false,
       movieDetailsSingle: {},
-      rating: 0
+      rating: 0,
+      videoURL: "empty"
     };
   },
+
   mounted() {
     let movies;
     if (process.browser) {
       movies = localStorage.getItem("moviesList");
     }
     if (!movies) {
-      this.$store.dispatch("setMovies", this.moviesList);
+      this.fetchData();
     }
   },
   methods: {
     onRateMovie(data) {
       this.updateMovie(data.id, "rating", data.rating);
       this.ratingModalState = true;
-
       this.rating = data.rating;
     },
     updateMovie(id, propName, value) {
@@ -69,7 +69,37 @@ export default {
       console.log(data);
       this.movieDetailsSingle = data;
       this.movieDetailsModalState = true;
+    },
+
+    fetchData: async function() {
+      try {
+        const res = await fetch(
+          "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=198c27d36e884f8226446016f0cfc7e4&append_to_response=videos"
+        );
+        const movies = await res.json();
+        movies.results = movies.results.map(obj => ({
+          ...obj,
+          rating: 0
+        }));
+
+        this.moviesList = movies.results;
+      } catch (e) {
+        console.log(e);
+      }
+      this.$store.dispatch("setMovies", this.moviesList);
+      console.log(JSON.parse(JSON.stringify(this.moviesList)));
+    },
+    fetchVideo() {
+      console.log("example  here");
     }
+    /* fetchVideo2: async function() {
+      const res = await fetch(
+        `https://api.themoviedb.org/3/movie/404368/videos?api_key=198c27d36e884f8226446016f0cfc7e4&language=en-US`
+      );
+      const movies = await res.json();
+      const result = await movies.results[0].key;
+      return result;
+    } */
   }
 };
 </script>
